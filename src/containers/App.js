e3bfe9b4e196fd79396c178c125516d406d6cfa1
wichaisw <wichai.sw19@gmail.com>
2020-06-16 15:37:28 +0700

    add components, containers folder, init DriverRoute

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DriverRoute from '../components/pages/DriverRoute';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <DriverRoute /> */}
+    </div>
+  );
+}
+
+export default App;
