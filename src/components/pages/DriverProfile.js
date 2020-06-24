3c6b09b53bac738f61f958661161c6714f4d0a1b
jthanawat <kla13thanawat@gmail.com>
2020-06-24 13:59:48 +0700

    add driver profile

--- src/components/pages/DriverProfile.js
+++ src/components/pages/DriverProfile.js
@@ -5,20 +5,33 @@ import { InfoCircleOutlined, UserOutlined, EyeInvisibleOutlined, EyeTwoTone, Que
 import '../pages/DriverProfile.css';
 import { Link, Redirect } from 'react-router-dom';
 import Navbar from '../Navbar';
+import HistoryCard from '../HistoryCard'
+import RoleButton from '../RoleButton'
 
 function DriverProfile() {
 
   return (
-  
-      <div className="driver-profile">
-        <div className="App__heading">
-          <h2>Dave Laravel</h2>
-        </div>
-        <div>
-          
-        </div>
 
+    <div className="driver">
+      <div className="App__heading">
+        <h2>Dave Laravel</h2>
       </div>
+
+      <div className='card__img-box'>
+        <img
+          src="https://cdn.mos.cms.futurecdn.net/vJvY6J485ReQFXAgx5DSJ3-650-80.jpg"
+          alt="profile-pic"
+          className='card__profile-img'
+        />
+      </div>
+
+      <div className="driver__display">
+        <h2>Profile information</h2>
+        <RoleButton />
+        <HistoryCard />
+      </div>
+
+    </div>
 
   )
 }
