5faeca84f5f709015711887be95c8e808301be5d
wichaisw <wichai.sw19@gmail.com>
2020-06-26 01:42:39 +0700

    add Log out & other link to Navbar, add shadow to NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,30 @@
 import React from "react";
 import "../styles/Navbar.css";
 
-function Navbar() {
-  return (
-    <div className="Navbar">
-      <nav>
-        <div className="">
+function Navbar(props) {
+
+
+  const navItems = () => {
+    switch(props.role) {
+      case 'guest': 
+        return(
           <ul>
             <li>
-              <a href="#">Search</a>
+              <a href="/">Login</a>
+            </li>
+            <li>
+              <a href="/register">Register</a>
+            </li>
+            <li>
+              <a href="/privacy-policy">Policy</a>
+            </li>
+          </ul>
+        )
+      case 'user':
+        return(
+          <ul>
+            <li>
+              <a href="/search-driver">Search</a>
             </li>
             <li>
               <a href="#">Inbox</a>
@@ -16,10 +32,33 @@ function Navbar() {
             <li>
               <a href="#">Profile</a>
             </li>
+            <li>
+              <a href='/' onClick={props.onLogOut}>Log out</a>
+            </li>
           </ul>
-        </div>
-      </nav>
-    </div>
+        )
+      default:
+        return(
+          <ul>
+            <li>
+              <a href="/">Login</a>
+            </li>
+            <li>
+              <a href="/register">Register</a>
+            </li>
+            <li>
+              <a href="/privacy-policy">Policy</a>
+            </li>
+          </ul>
+        )
+    }
+  }
+
+
+  return (
+    <nav className="Navbar">
+      {navItems()}
+    </nav>
   );
 }
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -39,7 +39,7 @@ function App() {
   return (
     <div className='App'>
       <PrivateRoute role={role} setRole={setRole} />
-      <Navbar onLogOut={onLogOut} />
+      <Navbar role={role} onLogOut={onLogOut} />
     </div>
   );
 }
