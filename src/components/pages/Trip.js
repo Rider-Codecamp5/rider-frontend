67eaf5999c5fc9d568ef98e71c1cf9044e969290
wichaisw <wichai.sw19@gmail.com>
2020-07-02 20:59:04 +0700

    add condition for rendering Trip onthe way page

--- src/components/pages/Trip.js
+++ src/components/pages/Trip.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import DriverMap from '../DriverMap';
 import HistoryCard from '../HistoryCard';
 import axios from '../../configs/axios';
+import { useHistory } from 'react-router-dom';
 
 import { useLoadScript } from '@react-google-maps/api';
 const libraries = ['places'];
@@ -13,6 +14,8 @@ function Trip(props) {
   const [ destination, setDestination] = useState({lat: null, lng: null});
   const [ isDriver, setIsDriver] = useState(false);
 
+  let history = useHistory();
+
   // need useEffect because tripData state is changed
   useEffect(() => {
     try{
@@ -23,16 +26,19 @@ function Trip(props) {
         let user = await axios.get('/driver/service/current');
         currentTrip = user.data.currentTrip;
         setTripData(currentTrip);
-        setOrigin({lat: currentTrip.from_lat, lng: currentTrip.from_lng});
-        setDestination({lat: currentTrip.to_lat, lng: currentTrip.to_lng});
 
-        // set Driver Personal Info according to currentTrip data
-        let result = await axios.get(`/user/get/${currentTrip.id}`); 
-        console.log('result', result);
-        setDriverInfo(result.data.userData);
+        if(currentTrip) {
+          setOrigin({lat: currentTrip.from_lat, lng: currentTrip.from_lng});
+          setDestination({lat: currentTrip.to_lat, lng: currentTrip.to_lng});
 
-        if(currentTrip.id === props.userInfo.id) {
-          setIsDriver(true);
+          // set Driver Personal Info according to currentTrip data
+          let result = await axios.get(`/user/get/${currentTrip.id}`); 
+          console.log('result', result);
+          setDriverInfo(result.data.userData);
+          
+          if(currentTrip.id === props.userInfo.id) {
+            setIsDriver(true);
+          }
         }
       }
 
@@ -53,40 +59,45 @@ function Trip(props) {
 
   return (
     <div className='route'>
-      <div className='App__heading'>
-        <h2>Current Trip</h2>
-      </div>
-      <div className='route__form' style={{display: 'flex', flexDirection: 'column'}}>
-        <span style={{fontSize: '1.2rem'}}>
-          <b>From: </b>{tripData.from}
-        </span>
-        <span style={{fontSize: '1.2rem'}}>
-          <b>To: </b>{tripData.to}
-        </span>
-        <div>
-          {console.log(origin)}
-          {console.log(destination)}
-          <DriverMap origin={origin} destination={destination} />
+    <div className='App__heading'>
+      <h2>Current Trip</h2>
+    </div>
+    {tripData.status === 'booked' 
+      ?
+        <div className='route__form' style={{display: 'flex', flexDirection: 'column'}}>
+          <span style={{fontSize: '1.2rem'}}>
+            <b>From: </b>{tripData.from}
+          </span>
+          <span style={{fontSize: '1.2rem'}}>
+            <b>To: </b>{tripData.to}
+          </span>
+          <div>
+            {console.log(origin)}
+            {console.log(destination)}
+            <DriverMap origin={origin} destination={destination} />
+          </div>
+          {console.log(driverInfo)}
+          <HistoryCard
+            id={driverInfo.id}
+            firstName={driverInfo.first_name}
+            lastName={driverInfo.last_name}
+            profilePic={driverInfo.profile_pic}
+            phoneNumber={driverInfo.phone_number}
+            from={tripData.from}
+            to={tripData.to}
+            carColor={tripData.car_color}
+            carModel={tripData.car_model}
+            driverLicense={tripData.driver_license}
+            seat={tripData.seating_capacity}
+            price={tripData.price}
+            dateTime={tripData.createdAt}
+            status={tripData.status}
+          />
+          {(isDriver) ? null : <button className='App__button'>Pay Now</button>}
         </div>
-        {console.log(driverInfo)}
-        <HistoryCard
-          id={driverInfo.id}
-          firstName={driverInfo.first_name}
-          lastName={driverInfo.last_name}
-          profilePic={driverInfo.profile_pic}
-          phoneNumber={driverInfo.phone_number}
-          from={tripData.from}
-          to={tripData.to}
-          carColor={tripData.car_color}
-          carModel={tripData.car_model}
-          driverLicense={tripData.driver_license}
-          seat={tripData.seating_capacity}
-          price={tripData.price}
-          dateTime={tripData.createdAt}
-          status={tripData.status}
-        />
-        {(isDriver) ? null : <button className='App__button'>Pay Now</button>}
-      </div>
+      : 
+        <h1 style={{textAlign: 'center', margin: '2rem'}}>You have no current trip</h1>
+    }
     </div>
   )
 }
