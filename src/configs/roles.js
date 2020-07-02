cf9fc9df516475386587dbe4398e71727ecf1114
wichaisw <wichai.sw19@gmail.com>
2020-07-02 22:36:14 +0700

    solve conflict in role.js config

--- src/configs/roles.js
+++ src/configs/roles.js
@@ -5,18 +5,12 @@ import DriverRegister from '../components/pages/DriverRegister';
 import UserRoute from '../components/pages/UserRoute';
 import PrivacyPolicy from '../components/pages/PrivacyPolicy';
 import History from '../components/pages/History';
-<<<<<<< HEAD
-import Setting from '../components/pages/UserSetting';
+import Settings from '../components/pages/Settings';
 import Profile from '../components/pages/DriverProfile';
 import RouteDetails from '../components/pages/RouteDetails';
 import Payment from '../components/pages/Payment';
 import PaymentResult from '../components/pages/PaymentResult';
-=======
-import Settings from '../components/pages/Settings';
-import Profile from '../components/pages/Profile'
-import RouteDetails from '../components/pages/RouteDetails';
 import Trip from '../components/pages/Trip';
->>>>>>> 75f963b4233bdef7eae6f466a0bc13a03b62a1c6
 
 const components = {
   Login: {
@@ -51,20 +45,13 @@ const components = {
     component: RouteDetails,
     url: '/driver/route-details/:id',
   },
-<<<<<<< HEAD
-  Setting: {
-    component: Setting,
-    url: '/setting',
-=======
   Settings: {
     component: Settings,
     url: '/settings',
->>>>>>> 75f963b4233bdef7eae6f466a0bc13a03b62a1c6
   },
   Profile: {
     component: Profile,
     url: '/profile',
-<<<<<<< HEAD
   },
   Payment: {
     component: Payment,
@@ -73,8 +60,6 @@ const components = {
   PaymentResult: {
     component: PaymentResult,
     url: '/payment-result',
-=======
->>>>>>> 75f963b4233bdef7eae6f466a0bc13a03b62a1c6
   },
   Trip: {
     component: Trip,
@@ -96,12 +81,8 @@ const configRoute = {
       components.DriverRegister,
       components.Settings,
       components.Profile,
-<<<<<<< HEAD
       components.Payment,
       components.PaymentResult,
-=======
-      components.Trip,
->>>>>>> 75f963b4233bdef7eae6f466a0bc13a03b62a1c6
     ],
     redirect: '/search-driver',
   },
@@ -114,12 +95,9 @@ const configRoute = {
       components.Settings,
       components.Profile,
       components.RouteDetails,
-<<<<<<< HEAD
       components.Payment,
       components.PaymentResult,
-=======
       components.Trip,
->>>>>>> 75f963b4233bdef7eae6f466a0bc13a03b62a1c6
     ],
     redirect: '/search-driver',
   },
