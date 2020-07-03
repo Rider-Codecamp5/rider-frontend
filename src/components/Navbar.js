21fa5cf7937f39e6de500055744f0fce0f7b1e33
wichaisw <wichai.sw19@gmail.com>
2020-07-03 15:55:08 +0700

    restyle settings, center AntD form using formItemLatout's labelCol

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,39 +2,48 @@ import React from "react";
 import "../styles/Navbar.css";
 
 function Navbar(props) {
+  
   const navItems = () => {
     switch(props.role) {
       case 'user':
-        return(
-          <ul>
+        try{
+          return(
+            <ul>
+              <li>
+                <a href='/search-driver'>Search</a>
+              </li>
+              <li onClick={props.showDrawer}>
+                <a href='#'>{props.userInfo.name}</a>
+              </li>
+              <li>
+                <a href='/' onClick={props.onLogOut}>Log out</a>
+              </li>
+            </ul>
+          )
+        } catch(err) {
+          props.onLogOut();
+        }
+      case 'driver':
+        try{
+          return(
+            <ul>
             <li>
               <a href='/search-driver'>Search</a>
             </li>
+            <li>
+              <a href='/driver/route'>Drive Now</a>
+            </li>
             <li onClick={props.showDrawer}>
-              <a>{props.userInfo.name}</a>
+              <a href='#'>{props.userInfo.name}</a>
             </li>
             <li>
               <a href='/' onClick={props.onLogOut}>Log out</a>
             </li>
           </ul>
-        )
-      case 'driver':
-        return(
-          <ul>
-          <li>
-            <a href='/search-driver'>Search</a>
-          </li>
-          <li>
-            <a href='/driver/route'>Drive Now</a>
-          </li>
-          <li onClick={props.showDrawer}>
-            <a>{props.userInfo.name}</a>
-          </li>
-          <li>
-            <a href='/' onClick={props.onLogOut}>Log out</a>
-          </li>
-        </ul>
-        )
+          )
+        } catch(err) {
+          props.onLogOut();
+        }
       default:
         return(
           <ul>
--- src/components/pages/History.js
+++ src/components/pages/History.js
@@ -2,17 +2,21 @@ import React, { useState } from 'react';
 import '../../styles/HistoryCard.css';
 import HistoryCard from '../HistoryCard';
 import RoleButton from '../RoleButton';
+import axios from '../../configs/axios';
 
-function History() {
+function History(props) {
+
   return (
     <div className='history'>
       <div className='App__heading'>
         <h2>Trip History</h2>
       </div>
       <div className='history__display'>
-        <RoleButton />
-        <HistoryCard name='Shinzo Abe' from='Siam' to='Silom' dateTime='Friday 55555555' carModel='Tesla T' price='300' history='Deny' />
-        <button className='App__button App__button--red'>Back</button>
+        <div className='route__form'>
+          <RoleButton />
+          <HistoryCard name='Shinzo Abe' from='Siam' to='Silom' dateTime='Friday 55555555' carModel='Tesla T' price='300' history='Deny' />
+          <button className='App__button App__button--red'>Back</button>
+        </div>
       </div>
     </div>
   )
