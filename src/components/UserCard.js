77f1abbb13798b687e8b47f2d83f4612429bb408
wichaisw <wichai.sw19@gmail.com>
2020-07-03 16:30:20 +0700

    add UserCard component, render Trip.js card according to role
    driver see passenger card, passenger see driver card

---
+++ src/components/UserCard.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import moment from 'moment';
+import {
+  CarOutlined,
+  CalendarOutlined,
+  PushpinOutlined,
+  TeamOutlined,
+  DollarOutlined,
+  PhoneOutlined,
+  NumberOutlined,
+} from '@ant-design/icons';
+import '../styles/HistoryCard.css';
+
+function UserCard(props) {
+  return (
+    <div className='card'>
+      <div className='card__content'>
+        <div className='card__profile'>
+          <div className='card__img-box'>
+            <img
+              src={props.profilePic}
+              alt={`${props.firstName} ${props.lastName}`}
+              className='card__profile-img'
+            />
+          </div>
+          <h3>{props.firstName} {props.lastName}</h3>
+        </div>
+        <div className='card__text'>
+          <span>
+            <b>From</b> {props.from}
+          </span>
+
+          <span>
+            <PushpinOutlined /> <b>To</b> {props.to}
+          </span>
+
+            <span>
+              <CalendarOutlined /> {moment(props.dateTime).format('MMMM Do YYYY')}
+            </span>
+          <span>
+            <TeamOutlined /> {props.seat} persons
+          </span>
+          <span>
+            <PhoneOutlined /> {props.phoneNumber}
+          </span>
+
+          <span className='card__price'>
+            <DollarOutlined /> Price {props.price} Baht
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserCard;
